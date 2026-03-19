8ce0527cceaefd978c0d2be0a1bfe53fa72c7587
MAGALI WALDFOGIEL <49553387@ort>
2026-03-19 08:02:50 -0300

    creacion metodos clase Escuela

--- Escuela.cs
+++ Escuela.cs
@@ -6,5 +6,54 @@ namespace TP0_5
     {
         public List<Alumno> Alumnos = new List<Alumno>();
     }
+    
+    public void AgregarAlumno(Alumno alumno)
+    {
+        alumnos.Add(alumno);
+    }
+
+    public Alumno BuscarporDNI(int dni)
+    {
+        int i = 0;
+
+        while (i < alumnos.Count)
+        {
+            if (alumnos[i].DNI == dni)
+            {
+                return alumnos[i];
+            }
+            i = i + 1;
+        }
+
+        return null;
+    }
+
+    public void mostrarTodos()
+    {
+        int i = 0;
+
+        while (i < alumnos.Count)
+        {
+            alumnos[i].mostrar();
+            i = i + 1;
+        }
+    }
+
+    public void mostrarLibres()
+    {
+        int i = 0;
+
+        while (i < alumnos.Count)
+        {
+            if (alumnos[i].CantidadFaltas > 15)
+            {
+                alumnos[i].mostrar();
+            }
+            i = i + 1;
+        }
+    }
 }
+
+
+
 
